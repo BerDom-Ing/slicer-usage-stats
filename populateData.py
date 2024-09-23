3edada680150066da65c31b8467def4301507d56
BernardoWork <ber.dom.engineer@gmail.com>
2024-09-23 16:51:38 -0300

    now uses csv and grouped data

--- populateData.py
+++ populateData.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import random
 from datetime import datetime, timedelta
-import json
+import os
+import geoip2.database
 
 # Function to generate random datetime strings within a range of months and days
 def random_datetime(start, end):
@@ -10,7 +11,7 @@ def random_datetime(start, end):
     return (start + timedelta(days=random_days, seconds=random_seconds)).isoformat()
 
 # Start and end datetime for generating random times
-start_time = datetime(2024, 1, 1)
+start_time = datetime(2019, 1, 1)
 end_time = datetime(2024, 12, 31)
 
 # List of functions to be used
@@ -94,15 +95,93 @@ modules = [
     "MedicalEducationSimulator"
 ]
 
+# Function to generate random IP addresses
+def generate_random_ip():
+    return ".".join(str(random.randint(0, 255)) for _ in range(4))
+
+# Mapping from ISO-3166 alpha3 to alpha2
+iso_alpha3_to_alpha2 = {
+    "AFG": "AF", "ALA": "AX", "ALB": "AL", "DZA": "DZ", "ASM": "AS", "AND": "AD", "AGO": "AO", "AIA": "AI",
+    "ATA": "AQ", "ATG": "AG", "ARG": "AR", "ARM": "AM", "ABW": "AW", "AUS": "AU", "AUT": "AT", "AZE": "AZ",
+    "BHS": "BS", "BHR": "BH", "BGD": "BD", "BRB": "BB", "BLR": "BY", "BEL": "BE", "BLZ": "BZ", "BEN": "BJ",
+    "BMU": "BM", "BTN": "BT", "BOL": "BO", "BES": "BQ", "BIH": "BA", "BWA": "BW", "BVT": "BV", "BRA": "BR",
+    "VGB": "VG", "IOT": "IO", "BRN": "BN", "BGR": "BG", "BFA": "BF", "BDI": "BI", "KHM": "KH", "CMR": "CM",
+    "CAN": "CA", "CPV": "CV", "CYM": "KY", "CAF": "CF", "TCD": "TD", "CHL": "CL", "CHN": "CN", "HKG": "HK",
+    "MAC": "MO", "CXR": "CX", "CCK": "CC", "COL": "CO", "COM": "KM", "COG": "CG", "COD": "CD", "COK": "CK",
+    "CRI": "CR", "CIV": "CI", "HRV": "HR", "CUB": "CU", "CUW": "CW", "CYP": "CY", "CZE": "CZ", "DNK": "DK",
+    "DJI": "DJ", "DMA": "DM", "DOM": "DO", "ECU": "EC", "EGY": "EG", "SLV": "SV", "GNQ": "GQ", "ERI": "ER",
+    "EST": "EE", "ETH": "ET", "FLK": "FK", "FRO": "FO", "FJI": "FJ", "FIN": "FI", "FRA": "FR", "GUF": "GF",
+    "PYF": "PF", "ATF": "TF", "GAB": "GA", "GMB": "GM", "GEO": "GE", "DEU": "DE", "GHA": "GH", "GIB": "GI",
+    "GRC": "GR", "GRL": "GL", "GRD": "GD", "GLP": "GP", "GUM": "GU", "GTM": "GT", "GGY": "GG", "GIN": "GN",
+    "GNB": "GW", "GUY": "GY", "HTI": "HT", "HMD": "HM", "VAT": "VA", "HND": "HN", "HUN": "HU", "ISL": "IS",
+    "IND": "IN", "IDN": "ID", "IRN": "IR", "IRQ": "IQ", "IRL": "IE", "IMN": "IM", "ISR": "IL", "ITA": "IT",
+    "JAM": "JM", "JPN": "JP", "JEY": "JE", "JOR": "JO", "KAZ": "KZ", "KEN": "KE", "KIR": "KI", "PRK": "KP",
+    "KOR": "KR", "KWT": "KW", "KGZ": "KG", "LAO": "LA", "LVA": "LV", "LBN": "LB", "LSO": "LS", "LBR": "LR",
+    "LBY": "LY", "LIE": "LI", "LTU": "LT", "LUX": "LU", "MKD": "MK", "MDG": "MG", "MWI": "MW", "MYS": "MY",
+    "MDV": "MV", "MLI": "ML", "MLT": "MT", "MHL": "MH", "MTQ": "MQ", "MRT": "MR", "MUS": "MU", "MYT": "YT",
+    "MEX": "MX", "FSM": "FM", "MDA": "MD", "MCO": "MC", "MNG": "MN", "MNE": "ME", "MSR": "MS", "MAR": "MA",
+    "MOZ": "MZ", "MMR": "MM", "NAM": "NA", "NRU": "NR", "NPL": "NP", "NLD": "NL", "ANT": "AN", "NCL": "NC",
+    "NZL": "NZ", "NIC": "NI", "NER": "NE", "NGA": "NG", "NIU": "NU", "NFK": "NF", "MNP": "MP", "NOR": "NO",
+    "OMN": "OM", "PAK": "PK", "PLW": "PW", "PSE": "PS", "PAN": "PA", "PNG": "PG", "PRY": "PY", "PER": "PE",
+    "PHL": "PH", "PCN": "PN", "POL": "PL", "PRT": "PT", "PRI": "PR", "QAT": "QA", "REU": "RE", "ROU": "RO",
+    "RUS": "RU", "RWA": "RW", "BLM": "BL", "SHN": "SH", "KNA": "KN", "LCA": "LC", "MAF": "MF", "SPM": "PM",
+    "VCT": "VC", "WSM": "WS", "SMR": "SM", "STP": "ST", "SAU": "SA", "SEN": "SN", "SRB": "RS", "SYC": "SC",
+    "SLE": "SL", "SGP": "SG", "SXM": "SX", "SVK": "SK", "SVN": "SI", "SLB": "SB", "SOM": "SO", "ZAF": "ZA",
+    "SGS": "GS", "SSD": "SS", "ESP": "ES", "LKA": "LK", "SDN": "SD", "SUR": "SR", "SJM": "SJ", "SWZ": "SZ",
+    "SWE": "SE", "CHE": "CH", "SYR": "SY", "TWN": "TW", "TJK": "TJ", "TZA": "TZ", "THA": "TH", "TLS": "TL",
+    "TGO": "TG", "TKL": "TK", "TON": "TO", "TTO": "TT", "TUN": "TN", "TUR": "TR", "TKM": "TM", "TCA": "TC",
+    "TUV": "TV", "UGA": "UG", "UKR": "UA", "ARE": "AE", "GBR": "GB", "USA": "US", "UMI": "UM", "URY": "UY",
+    "UZB": "UZ", "VUT": "VU", "VEN": "VE", "VNM": "VN", "VIR": "VI", "WLF": "WF", "ESH": "EH", "YEM": "YE",
+    "ZMB": "ZM", "ZWE": "ZW", "XKX": "XK"
+}
+
+# Reverse the mapping to get alpha2 to alpha3
+iso_alpha2_to_alpha3 = {v: k for k, v in iso_alpha3_to_alpha2.items()}
+
+def convert_alpha2_to_alpha3(alpha2_code):
+    """
+    Convert ISO-3166 alpha2 code to ISO-3166 alpha3 code.
+    
+    Parameters:
+    alpha2_code (str): The ISO-3166 alpha2 code.
+    
+    Returns:
+    str: The corresponding ISO-3166 alpha3 code.
+    """
+    return iso_alpha2_to_alpha3.get(alpha2_code, None)
+
+
+# Function to get location from IP using GeoIP database
+def get_location_from_ip(ip, db_path):
+    with geoip2.database.Reader(db_path) as reader:
+        try:
+            response = reader.city(ip)
+            country = response.country.name if response.country.name else "Unknown"
+            country_code = response.country.iso_code if response.country.iso_code else "Unknown"
+            city = response.city.name if response.city.name else "Unknown"
+            return country, country_code, city
+        except geoip2.errors.AddressNotFoundError:
+            return "Unknown", "Unknown", "Unknown"
+
+# Path to GeoIP database
+current_dir = os.path.dirname(__file__)
+db_path = os.path.join(current_dir, 'GeoLite2-City_20240809', 'GeoLite2-City.mmdb')
+
 # Generate data for each medical module
 data = []
 for module in modules:
-    num_functions = random.randint(1, 10)  # Random number of functions (1 to 10)
+    num_functions = random.randint(1, 500)  # Random number of functions (1 to 10)
     for _ in range(num_functions):
+        ip = generate_random_ip()
+        country, country_code, city = get_location_from_ip(ip, db_path)
+        country_code_alpha3 = convert_alpha2_to_alpha3(country_code)  # Convert to alpha3
         data.append({
             "function": random.choice(functions),
             "time": random_datetime(start_time, end_time),
-            "selected_module": module
+            "selected_module": module,
+            "country": country,
+            "country_code": country_code_alpha3,
+            "city": city
         })
 
 # Convert to DataFrame
--- stats.js
+++ stats.js
@@ -1,275 +1,195 @@
-'use strict';
+// Load required libraries and data
+Promise.all([
+    d3.csv('data.csv'),
+    d3.json('world.geojson')
+]).then(function([data, worldData]) {
+    // Parse dates and create a numerical representation for crossfilter
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    let expandedData = [];
+    data.forEach(d => {
+        d.date = parseDate(d.day);
+        d.dateNum = d.date.getTime();
+        const times = parseInt(d.times, 10);
+        for (let i = 0; i < times; i++) {
+            expandedData.push({ ...d });
+        }
+    });
 
-var app = function() {
-	var exports = {};
+    // Initialize Crossfilter
+    const cf = crossfilter(expandedData);
 
-    function main(defaults) {
-        blankContent(true);
+    // Create dimensions
+    const dateDim = cf.dimension(d => d.date);
+    const moduleDim = cf.dimension(d => d.component);
+    const functionDim = cf.dimension(d => d.event);
+    const countryDim = cf.dimension(d => d.country_code);
+    const cityDim = cf.dimension(d => d.city);
 
-        var spinner = createSpinner().start();
-        d3.json(defaults.downloadDataFile, function(err, data) {
-            if(err) {
-                d3.json(defaults.fallbackDataFile, function(err, data) {
-                    console.error('primary data file not found, reverting to fallback data.')
-                    processData(data);
-                });
-            } else {
-                processData(data);
-            }
-            blankContent(false);
-            spinner.stop();
+
+    // Create groups
+    const dateGroup = dateDim.group(d3.timeDay);
+    const moduleGroup = moduleDim.group().reduceCount();
+    const functionGroup = functionDim.group().reduceCount();
+    const countryGroup = countryDim.group().reduceCount();
+    const cityGroup = cityDim.group().reduceCount();
+
+    // Set up the charts
+    const timeChart = dc.barChart("#time-chart");
+    const moduleChart = dc.barChart("#module-chart");
+    const functionChart = dc.rowChart("#function-chart");
+    const cityChart = dc.barChart("#city-chart");
+
+    // Time chart
+    timeChart
+        .width(900)
+        .height(200)
+        .margins({top: 10, right: 10, bottom: 20, left: 40})
+        .dimension(dateDim)
+        .group(dateGroup)
+        .x(d3.scaleTime().domain(d3.extent(data, d => d.date)))
+        .round(d3.timeDay.round)
+        .xUnits(d3.timeDays)
+        .elasticY(true)
+        .renderHorizontalGridLines(true)
+        .brushOn(true);
+
+    // Module chart (horizontal bar chart)
+    moduleChart
+        .width(900)
+        .height(300)
+        .margins({top: 20, right: 20, bottom: 100, left: 40})
+        .dimension(moduleDim)
+        .group(moduleGroup)
+        .x(d3.scaleBand())
+        .xUnits(dc.units.ordinal)
+        .elasticY(true)
+        .ordering(d => -d.value)
+        .renderHorizontalGridLines(true)
+        .on('renderlet', function(chart) {
+            chart.selectAll('g.x text')
+                .attr('transform', 'translate(-10,10) rotate(270)');
         });
-    }
 
-    function versionStringToInt(s) {
-	return s.split('.').reduce((a, v, i) => a + parseInt(v)*Math.pow(1000, 2-i), 0);
-    }
+    // Function chart
+    functionChart
+    .width(400)
+    .height(300)
+    .margins({top: 20, left: 10, right: 10, bottom: 20})
+    .dimension(functionDim)
+    .group(functionGroup)
+    .elasticX(true)
+    .ordering(d => -d.value)
+    .title(d => `${d.key}: ${d.value}`)
+    .on('renderlet', function(chart) {
+        chart.selectAll('g.row text')
+            .style('fill', 'black');
+    });
 
-    function processData(data) {
-    	var refData = _.map(data.access, function(x) { return referenceRecord(x, data); });
-    	var accessf = crossfilter(refData);
-    	var allAccess = accessf.groupAll();
+    // City chart
+    cityChart
+    .width(400)
+    .height(300)
+    .margins({top: 20, right: 20, bottom: 100, left: 40})
+    .dimension(cityDim)
+    .group(cityGroup)
+    .x(d3.scaleBand())
+    .xUnits(dc.units.ordinal)
+    .elasticY(true)
+    .ordering(d => -d.value)
+    .renderHorizontalGridLines(true)
+    .on('renderlet', function(chart) {
+        chart.selectAll('g.x text')
+            .attr('transform', 'translate(-10,10) rotate(270)');
+    });    
 
-    	var chartColorValues = colorbrewer.Set3[12];
-    	var dateTextFormat = d3.time.format('%x');
+    // World Map
+    const width = 960;
+    const height = 500;
 
-    	var byOS = accessf.dimension(function(x) { return x[0].os; });
-    	var byMinorVersion = accessf.dimension(function(x) { return x[0].version.split('.').slice(0, 2).join('.'); });
-    	var byStable = accessf.dimension(function(x) { return !!x[0].stable ? "release": "nightly"; });
-    	var bySubregion =  accessf.dimension(function(x) { return x[3][1]; });
-    	var byCountry =  accessf.dimension(function(x) { return x[3][0]; });
-    	var byDay = accessf.dimension(function(x) { return d3.time.day(x[1]); });
-	var sortByVersion = function(x) { return versionStringToInt(x.key); };
+    const svg = d3.select("#map-chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-    	var dayRange = expandDateRange([byDay.bottom(1)[0][1],
-    		byDay.top(1)[0][1]],
-    		'day');
-    	var groupByOS = byOS.group();
-    	var groupBySubregion = bySubregion.group();
+    const projection = d3.geoMercator()
+        .scale(130)
+        .center([0,20])
+        .translate([width / 2, height / 2]);
 
-    	var groupByDay = byDay.group();
+    const path = d3.geoPath().projection(projection);
 
-    	var groupByMinorVersion = byMinorVersion.group();
-    	var groupByMinorVersion = byMinorVersion.group();
-    	var groupByCountry = byCountry.group();
-    	var groupByStable = byStable.group();
+    const colorScale = d3.scaleThreshold()
+        .domain([1, 5, 10, 20, 50, 100])
+        .range(d3.schemeBlues[7]);
 
-    	var versionChart = dc.rowChart("#version-chart");
-    	var osChart = dc.rowChart("#os-chart");
-    	var regionChart = dc.rowChart("#region-chart");
-    	var countryChart = dc.rowChart("#country-chart");
-    	var stableChart = dc.rowChart('#stable-chart');
-    	var monthChart = dc.lineChart('#month-chart');
-
-    	var dateRangeEl = $('#daterange');
-    	var dateRangeContentEl = $('#daterange-content');
-
-    	var datepicker = dateRangeEl.daterangepicker({
-    		startDate: dayRange[0],
-    		endDate: dayRange[1],
-    		minDate: dayRange[0],
-    		ranges: {
-    			"all downloads": dayRange,
-    			"past week": [moment().subtract(1, 'week').startOf('day'), moment().endOf('day')],
-    			"past month": [moment().subtract(1, 'month').startOf('day'), moment().endOf('day')],
-    			"past year": [moment().subtract(1, 'year').startOf('day'), moment().endOf('day')],
-    		}
-    	}, function() {});
-
-    	function setDateRangeText(start, end) {
-    		dateRangeContentEl.html(start.format('ll') + ' - ' + end.format('ll'));
-    	}
-
-    	datepicker.on('apply.daterangepicker', function(ev, picker) {
-    		setDateRangeText(picker.startDate, picker.endDate);
-    		monthChart.x(d3.time.scale().domain([picker.startDate, picker.endDate]));
-            byDay.filterRange(expandDateRange([picker.startDate, picker.endDate], 'day'));
-    		monthChart.render();
-    		dc.redrawAll();
-    	}).on('show.daterangepicker', function(ev, picker) {
-            setDateRangeText(picker.startDate, picker.endDate);
-    	});
-
-    	setDateRangeText(dayRange[0], dayRange[1]);
-
-
-    	exports.versionChart = versionChart;
-    	exports.osChart = osChart;
-    	exports.regionChart = regionChart;
-    	exports.countryChart = countryChart;
-    	exports.stableChart = stableChart;
-    	exports.monthChart = monthChart;
-
-    	versionChart.width(320)
-    	.height(250)
-    	.ordinalColors([chartColorValues[0]])
-    	.dimension(byMinorVersion)
-    	.group(groupByMinorVersion)
-	.ordering(sortByVersion)
-    	.elasticX(true)
-    	.label(function(x) { return keyAndPercentValueLabel(allAccess.value(), x) })
-    	.xAxis().ticks(4);
-
-    	osChart.width(320)
-    	.height(130)
-    	.ordinalColors([chartColorValues[6]])
-    	.dimension(byOS)
-    	.group(groupByOS)
-    	.label(osLabel)
-    	.elasticX(true)
-    	.label(function(x) { return osPercentLabel(allAccess.value(), x) })
-    	.xAxis().ticks(4);
-
-    	stableChart.width(320)
-    	.height(100)
-    	.ordinalColors([chartColorValues[5]])
-
-    	.dimension(byStable)
-    	.group(groupByStable)
-    	.label(function(x) { return keyAndPercentValueLabel(allAccess.value(), x) })
-    	.elasticX(true)
-    	.xAxis().ticks(4);
-
-    	regionChart.width(320)
-    	.height(550)
-    	.ordinalColors([chartColorValues[3]])
-    	.dimension(bySubregion)
-    	.group(groupBySubregion)
-    	.label(keyAndValueLabel)
-    	.elasticX(true)
-    	.cap(18)
-    	.ordering(function(d) { return -d.value; })
-    	.xAxis().ticks(4);
-
-    	countryChart.width(320)
-    	.height(550)
-    	.ordinalColors([chartColorValues[4]])
-    	.dimension(byCountry)
-    	.group(groupByCountry)
-    	.label(keyAndValueLabel)
-    	.elasticX(true)
-    	.cap(18)
-    	.ordering(function(d) { return -d.value; })
-    	.xAxis().ticks(4);
-
-    	monthChart.width(900) 
-   	.height(175)
-    	.ordinalColors(['#5555aa'])
-    	.elasticY(true)
-    	.dimension(byDay)
-    	.group(groupByDay)
-    	.brushOn(false)
-    	.x(d3.time.scale().domain(dayRange))
-    	.renderlet(function(chart) {
-    		var top = chart.dimension().top(1)[0];
-    		var bot = chart.dimension().bottom(1)[0];
-    		outputDateRange(dateTextFormat(bot[1]), dateTextFormat(top[1]));
-    	})
-    	.interpolate('basis')
-    	.round(d3.time.day.round)
-    	.yAxis().ticks(3);
-
-    	dc.dataCount('.dc-data-count')
-    	.dimension(accessf)
-    	.group(allAccess);
-
-	// these calls are required to set up the initial state of the visualization.
-	dc.filterAll();
-	dc.renderAll();
-	dc.redrawAll();
-}
-
-    function blankContent(state) {
-        d3.selectAll('.initially-hidden').classed('initially-hidden', state);
-    }
-
-    function createSpinner() {
-        var _spinner = new Spinner().spin();
-        return {
-            start: function() {
-                document.querySelector('#all-of-page').appendChild(_spinner.el);
-                return _spinner;
-            },
-            stop: function() {
-                _spinner.stop();
-                return _spinner;
-            }
-        }
-    }
-
-    function chartReset(chart) {
-        chart.filterAll();
-        dc.redrawAll();
-    }
-
-
-    // expand the date range by a fixed amount of time both before
-    // and after in order to be sure to include endpoints in the data
-    // range.
-    function expandDateRange(range, amt) {
-        return [moment(range[0]).startOf(amt),
-        moment(range[1]).endOf(amt)];
-    }
-
-    // Returns a truth function for matching selected versions.
-    // Version numbers with only major or major.minor numbers match
-    // all appropriate major.minor.release numbers.
-    function versionSelectFilter(v) {
-        return function(d) {
-            if (v === '*') { return true };
-            var matchVersion = v.split('.');
-            var testVersion = d.split('.');
-            var testLength = Math.min(matchVersion.length, testVersion.length);
-            for(var i = testLength - 1; i >=0; i--) {
-                if(matchVersion[i] != testVersion[i]) {
-                    return false;
+    function updateMap() {
+        const countryData = countryGroup.all().reduce((acc, d) => {
+            acc[d.key] = d.value;
+            return acc;
+        }, {});
+    
+        const currentFilter = countryDim.currentFilter();
+        const selectedCountries = currentFilter ? (Array.isArray(currentFilter) ? currentFilter : [currentFilter]) : [];
+    
+        svg.selectAll("path")
+            .data(worldData.features)
+            .join("path")
+            .attr("d", path)
+            .attr("fill", d => {
+                const count = countryData[d.id] || 0;
+                return colorScale(count);
+            })
+            .attr("stroke", d => selectedCountries.includes(d.id) ? "red" : "#fff")
+            .attr("stroke-width", d => selectedCountries.includes(d.id) ? 2 : 0.5)
+            .on("click", function(event, d) {
+                const country = d.id;
+                if (selectedCountries.includes(country)) {
+                    countryDim.filterAll(); // Clear the filter if it's already applied
+                } else {
+                    countryDim.filter(country);
                 }
-            }
-            return true;
-        }
+                dc.redrawAll();
+                updateMap();
+            })
+            .on("mouseover", function(event, d) {
+                if (!selectedCountries.includes(d.id)) {
+                    d3.select(this).attr("stroke", "black").attr("stroke-width", 1.5);
+                }
+                const count = countryData[d.id] || 0;
+                d3.select("#tooltip")
+                    .style("opacity", 1)
+                    .html(`${d.id}: ${count}`)
+                    .style("left", (event.pageX + 10) + "px")
+                    .style("top", (event.pageY - 15) + "px");
+            })
+            .on("mouseout", function(event, d) {
+                if (!selectedCountries.includes(d.id)) {
+                    d3.select(this).attr("stroke", "#fff").attr("stroke-width", 0.5);
+                }
+                d3.select("#tooltip").style("opacity", 0);
+            });
+    }
+    // Add reset button
+    d3.select('#reset-button')
+        .on('click', function() {
+            dc.filterAll();
+            countryDim.filterAll();
+            cityDim.filterAll();
+            dc.renderAll();
+            updateMap();
+        });
+
+    // Render the charts and map
+    dc.renderAll();
+    updateMap();
+
+    // Update map when any chart is filtered
+    function onFiltered() {
+        updateMap();
     }
 
-    // converts a "data and id" representation to a reference-based one.
-    function referenceRecord(a, data) {
-        var result = [
-        data.bitstream[a[0]],
-        new Date(a[1] + 'Z'),
-        a[2],
-        data.countryCode[a[2]],
-        data.location[a[3]]
-        ];
-        return result;
-    }
-
-    // various formatting helpers
-    function keyAndValueLabel(d) {
-        return "" + d.key + "  (" + d.value + ")";
-    }
-
-    function keyAndPercentValueLabel(total_value, d) {
-        return "" + d.key + "  (" + d3.round(d.value/total_value*100.0, 0) + "%)";
-    }
-
-    var osLabelDict = {
-        linux: "Linux",
-        macosx: "Mac",
-        win: "Windows"
-    };
-
-    function osLabel(d) {
-        return keyAndValueLabel({key: osLabelDict[d.key], value: d.value});
-    }
-
-    function osPercentLabel(total_value, d) {
-        return keyAndPercentValueLabel(total_value, {key: osLabelDict[d.key], value: d.value});
-    }
-
-    function outputDateRange(low, high) {
-        d3.select('#date-low').text(low);
-        d3.select('#date-high').text(high);
-    }
-
-    exports.chartReset = chartReset;
-    exports.main = main;
-    return exports;
-}();
+    timeChart.on("filtered", onFiltered);
+    moduleChart.on("filtered", onFiltered);
+    functionChart.on("filtered", onFiltered);
+    cityChart.on("filtered", onFiltered);
+});
